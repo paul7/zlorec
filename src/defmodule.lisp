3c04017a4655892dc76196a9421d7e43992a26eb
Paul A. Anokhin <paul7@redeye.(none)>
2010-09-23 02:01:48 +0400

    integrate lweb as a submodule

--- src/defmodule.lisp
+++ src/defmodule.lisp
@@ -4,6 +4,22 @@
 
 (in-package #:zlorec)
 
+(restas:define-submodule #:lweb-viewer (#:lweb)
+  (*default-render-method* #'(lambda (obj)
+			      (closure-template.standard:xhtml-strict-frame
+			       (list :title (getf obj :title)
+				     :body (lweb.view:main-view 
+					    (list 
+					     :login (restas:genurl 'login-form 
+								   :return (hunchentoot:url-encode (hunchentoot:request-uri*)))
+					     :impl (list :type (lisp-implementation-type)
+							 :version (lisp-implementation-version))
+					     :index (restas:genurl 'message-list)
+					     :body (restas:render-object 
+						    (find-package ':lweb.view)
+						    obj))))))))
+
+
 (defun recompile-templates ()
   (closure-template:compile-template :common-lisp-backend
 				     (merge-pathnames "src/graph.tmpl"
