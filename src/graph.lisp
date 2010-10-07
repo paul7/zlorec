0f00f96cc965d68400b650477b3ca596bdd70686
Paul A. Anokhin <paul7@paul7-dell.(none)>
2010-10-07 20:09:05 +0400

    minor svg output refactoring

--- src/graph.lisp
+++ src/graph.lisp
@@ -13,8 +13,8 @@
 			    :method :post
 			    :requirement #'(lambda () (hunchentoot:post-parameter "send")))
   (list :graph (restas:genurl 'user-activity 
-			      :user (hunchentoot:post-parameter "user")
-			      :unit (hunchentoot:post-parameter "unit")
+			      :user   (hunchentoot:post-parameter "user")
+			      :unit   (hunchentoot:post-parameter "unit")
 			      :amount (hunchentoot:post-parameter "amount"))
 	:return (restas:genurl 'graph-form)))
 
@@ -27,31 +27,38 @@
 (defparameter *graph-height* 400)
 (defparameter *graph-width* 400)
 
+(defun render-svg-bar-graph (values &key (title "") subscripts)
+  (declare (ignore title subscripts))
+  (let* ((amount (length values))
+	 (max-value (+ 0.1 (apply #'max values)))
+	 (bar-width (floor (/ *graph-width* amount)))
+	 (scaled (iter 
+		   (for offset from 0)
+		   (for val in values)
+		   (collect
+		       (list :y      (floor (* (* val 0.9) 
+					       (/ *graph-height* max-value)))
+			     :x      bar-width
+			     :offset (* offset bar-width)
+			     :bottom *graph-height*))
+			 values)))
+    (list :values scaled
+	  :width  (* amount bar-width)
+	  :height *graph-height*)))
+
 (restas:define-route user-activity ("useract.svg/:user/:unit/:amount"
 				    :parse-vars (list :amount #'parse-integer
-						      :unit #'validate-unit
-						      :user #'hunchentoot:url-decode)
-				    :render-method #'zlorec.view:user-activity
+						      :unit   #'validate-unit
+						      :user   #'hunchentoot:url-decode)
+				    :render-method #'zlorec.view:svg-bar-graph
 				    :content-type "image/svg+xml")
   (if (and (plusp (length user))
 	   (plusp amount))
-      (let* ((values (get-user-activity user :unit unit :amount amount))
-	     (max-value (+ 0.1 (apply #'max values)))
-	     (bar-width (floor (/ *graph-width* amount)))
-	     (offset -1)
-	     (scaled (mapcar #'(lambda (val)
-				 (list :y (floor (* (* val 0.9) (/ *graph-height* max-value)))
-				       :x bar-width
-				       :offset (* (incf offset) bar-width)
-				       :height *graph-height*))
-			     values)))
-	(list :values scaled
-	      :width *graph-width*
-	      :height *graph-height*))
+      (render-svg-bar-graph (get-user-activity user :unit unit :amount amount))
       (list :error "bad parameters")))
 		      
 (restas:define-route retrieved ("zlorec")
-  (list :lastid (max-message-id)
+  (list :lastid  (max-message-id)
 	:lastbad (max-message-id :classes '(bad-message))
-	:msgnum (message-count)
-	:badnum (message-count :classes '(bad-message))))
+	:msgnum  (message-count)
+	:badnum  (message-count :classes '(bad-message))))
