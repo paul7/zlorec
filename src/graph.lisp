c9652c431e392f414bc33a13e7eae7d404e87479
Paul A. Anokhin <paul7@paul7-dell.(none)>
2010-10-19 21:11:28 +0400

    subscripts and marks

--- src/graph.lisp
+++ src/graph.lisp
@@ -24,27 +24,56 @@
     ((equalp unit "month") :month)
     ((equalp unit "year")  :year)))
 
-(defparameter *graph-height* 400)
-(defparameter *graph-width* 400)
+(defparameter *graph-height* 500)
+(defparameter *graph-width* 500)
+(defparameter *graph-sections* 5)
+(defparameter *graph-border-part* 1/10)
+
+(defun scale-unit (max-value)
+  (let ((ten-power (max 1 (expt 10 
+				(floor (log (/ max-value *graph-sections*) 10))))))
+    (* ten-power 
+       (ceiling (/ max-value *graph-sections* ten-power)))))
 
 (defun render-svg-bar-graph (values &key (title "") subscripts)
-  (declare (ignore title subscripts))
   (let* ((amount (length values))
-	 (max-value (+ 0.1 (apply #'max values)))
-	 (bar-width (floor (/ *graph-width* amount)))
+	 (max-value (apply #'max values))
+	 (border-width (* *graph-border-part* *graph-width*))
+	 (border-height (* *graph-border-part* *graph-height*))
+	 (width (- *graph-width* (* 2 border-width)))
+	 (height (- *graph-height* (* 2 border-height)))
+	 (bar-width (floor (/ width amount)))
+	 (scale-unit (scale-unit max-value))
+	 (scale-max (* *graph-sections* scale-unit))
+	 (scale (/ height scale-max))
+	 (subscripts (append subscripts (make-list (max 0 (- (length values)
+							     (length subscripts)))
+						   :initial-element "")))
 	 (scaled (iter 
 		   (for offset from 0)
 		   (for val in values)
+		   (for sub in subscripts)
 		   (collect
-		       (list :y      (floor (* (* val 0.9) 
-					       (/ *graph-height* max-value)))
-			     :x      bar-width
-			     :offset (* offset bar-width)
-			     :bottom *graph-height*))
-			 values)))
-    (list :values scaled
-	  :width  (* amount bar-width)
-	  :height *graph-height*)))
+		       (list :y         (floor (* val scale))
+			     :x         bar-width
+			     :offset    (+ border-width (* offset bar-width))
+			     :bottom    (+ border-height height)
+			     :subscript sub))
+			 values))
+	 (marks (iter 
+		  (for mark from 1 below *graph-sections*)
+		  (collect 
+		      (let ((mark (* scale-unit mark)))
+			(list :mark mark
+			      :y    (- *graph-height* border-height (* scale mark))))))))
+    (print subscripts)
+    (list :values  scaled
+	  :marks   marks
+	  :bwidth  border-width
+	  :bheight border-height
+	  :width   (+ (* 2 border-width)  (* amount bar-width))
+	  :height  *graph-height*
+	  :title   title)))
 
 (pm:define-memoized-route user-activity ("useract.svg/:user/:unit/:amount"
 					 :parse-vars (list :amount #'parse-integer
@@ -55,10 +84,12 @@
   (if (and (plusp (length user))
 	   (plusp amount))
       (values 
-       (render-svg-bar-graph (get-user-activity user :unit unit :amount amount))
-       '(1 :hour))
+       (render-svg-bar-graph (get-user-activity user :unit unit :amount amount) 
+			     :title      user
+			     :subscripts (iota amount :start (- amount))) 
+       '(1 :sec))
       (list :error "bad parameters")))
-		      
+
 (restas:define-route retrieved ("zlorec")
   (list :lastid  (max-message-id)
 	:lastbad (max-message-id :classes '(bad-message))
