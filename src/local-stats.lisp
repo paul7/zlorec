7f7454485d649324cdfac9dcf3841948be52730b
Paul A. Anokhin <paul7@paul7-dell.(none)>
2010-10-25 21:30:34 +0400

    calendar intervals

--- src/local-stats.lisp
+++ src/local-stats.lisp
@@ -28,6 +28,36 @@
 			    (getf range :to))
 	(db-user-query user))))
 
+(defparameter *date-units* `((:year  ,#'timestamp-year)
+			     (:month ,#'timestamp-month)
+			     (:day   ,#'timestamp-day)
+			     (:hour  ,#'timestamp-hour)))
+
+(defun user-post-number-around (&key user date unit)
+  (let ((flag nil))
+    (flet ((insignificant (cur-unit)
+	     (prog1
+		 flag
+	       (if (eq unit cur-unit)
+		   (setf flag t)))))
+      (with-connection *db-spec* 
+	(query (:select (:count :*) :from 'message
+			:where (:and (:or (not user)
+					  (:= :author (or user "")))
+				     (:or (insignificant :year)
+					   (:= (:extract :year 'date) 
+					       (timestamp-year date)))
+				     (:or (insignificant :month)
+					   (:= (:extract :month 'date) 
+					       (timestamp-month date)))
+				     (:or (insignificant :day)
+					   (:= (:extract :day 'date) 
+					       (timestamp-day date)))
+				     (:or (insignificant :hour)
+					   (:= (:extract :hour 'date) 
+					       (timestamp-hour date)))))
+	       :single)))))
+			    
 (defun total-post-number (range)
   (with-connection *db-spec* 
     (db-total-date-query (getf range :from)
--- src/user-activity.lisp
+++ src/user-activity.lisp
@@ -1,12 +1,8 @@
 (in-package #:zlorec)
 
 (defun get-total-activity (&key (unit :hour) (amount 24))
-  (with-connection *db-spec*
-    (iter 
-      (for period from amount downto 1)
-      (collect (total-post-number
-		(list :from (timestamp- (now) period unit)
-		      :to   (timestamp- (now) (1- period) unit)))))))
+  (get-user-activity-calendar :unit unit 
+			      :amount amount))
 
 (defun get-user-activity (user &key (unit :month) (amount 12))
   (with-connection *db-spec*
@@ -15,4 +11,11 @@
       (collect (user-post-number user 
 				 :range (list :from (timestamp- (now) period unit)
 					      :to   (timestamp- (now) (1- period) unit)))))))						   
-  
+(defun get-user-activity-calendar (&key user (now (now)) (unit :month) (amount 12))
+  (with-connection *db-spec* 
+    (iter
+      (for i from (1- amount) downto 0)
+      (collect (user-post-number-around :user user 
+					:date (timestamp- now i unit) 
+					:unit unit)))))
+
