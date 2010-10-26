4e7f2fdc1ba0cfbad6c96f8425f0cbb318867e30
Paul A. Anokhin <paul7@paul7-dell.(none)>
2010-10-26 19:05:38 +0400

    Revert experiments
    This reverts commit 985722300b2bcd42a6c281b4668e381414501dd0.

--- src/user-activity.lisp
+++ src/user-activity.lisp
@@ -11,18 +11,11 @@
       (collect (user-post-number user 
 				 :range (list :from (timestamp- (now) period unit)
 					      :to   (timestamp- (now) (1- period) unit)))))))						   
-(defun timestamp-next-unit (unit)
-  (ecase unit
-    (:year  :month)
-    (:month :day)
-    (:day   :hour)
-    (:hour  :min)))
-
 (defun get-user-activity-calendar (&key user (now (now)) (unit :month) (amount 12))
-  (let ((now (timestamp-maximize-part now (timestamp-next-unit unit))))
-    (with-connection *db-spec*
-      (iter 
-	(for period from amount downto 1)
-	(collect (user-post-number user 
-				   :range (list :from (timestamp- now period unit)
-						:to   (timestamp- now (1- period) unit))))))))		
+  (with-connection *db-spec* 
+    (iter
+      (for i from (1- amount) downto 0)
+      (collect (user-post-number-around :user user 
+					:date (timestamp- now i unit) 
+					:unit unit)))))
+
