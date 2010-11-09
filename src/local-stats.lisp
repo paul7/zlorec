1a3d6bd18586c7d08f916732079d9e58fcb4c7af
Paul A. Anokhin <paul7@paul7-dell.(none)>
2010-11-09 20:05:35 +0300

    earliest post date

--- src/local-stats.lisp
+++ src/local-stats.lisp
@@ -20,6 +20,15 @@
 		     (:< :date '$2)))
   :single)
 
+(defprepared db-earliest-post
+    (:select (:min 'date) :from 'message)
+  :single)
+
+(defprepared db-earliest-post-user
+    (:select (:min 'date) :from 'message
+	     :where (:= :author '$1))
+  :single)
+
 (defun user-post-number (user &key range)
   (if range
       (db-user-date-query user
@@ -31,3 +40,10 @@
   (db-total-date-query (getf range :from)
 		       (getf range :to)))
     
+(defun earliest-post (&optional user)
+  (let ((date (if user
+		  (db-earliest-post-user user)
+		  (db-earliest-post))))
+    (if (typep date 'db-null)
+	(unix-to-timestamp 0)
+	date)))
