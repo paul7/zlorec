bd15444bb3ae9b984ae7cdd1a503bdb81b93c985
Paul A. Anokhin <paul7@paul7-dell.(none)>
2010-11-10 17:11:15 +0300

    stop fetching data before user`s first message

--- src/user-activity.lisp
+++ src/user-activity.lisp
@@ -8,24 +8,28 @@
     (:hour  (timestamp-hour timestamp))
     (:min   (timestamp-minute timestamp))))
 
-(defmacro define-activity (name (&rest args) &body body)
+(defmacro define-activity (name (&rest args) time-limit &body body)
   `(defun ,name (&key (now (now)) unit amount ,@args)
      (with-connection *db-spec*
        (iter 
 	 (for period from amount downto 1)
-	 (for date initially (timestamp- now (1- amount) unit) then (timestamp+ date 1 unit))
+	 (for date initially now then (timestamp- date 1 unit))
+	 ,(if time-limit
+	      `(progn
+		 (with start = (progn ,time-limit))
+		 (until (timestamp< date start))))
 	 (collect (progn ,@body)
-	   into numbers)
+	   into numbers at beginning)
 	 (collect (timestamp-subscript-part date unit)
-	   into labels)
+	   into labels at beginning)
 	 (finally (return (values numbers labels)))))))
 
-(define-activity get-total-activity ()
+(define-activity get-total-activity () (earliest-post)
   (total-post-number
    (list :from (timestamp- date 1 unit)
 	 :to   date)))
 
-(define-activity get-user-activity (user)
+(define-activity get-user-activity (user) (earliest-post user)
   (user-post-number user :range (list :from (timestamp- date 1 unit)
 				      :to   date)))
 	  
